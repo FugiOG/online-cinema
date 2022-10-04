37158ff71404bb351c280690d02382c401e167eb
Fugi <rememberov.andrei@yandex.ru>
2022-10-04 12:51:56 +0300

    feat: jwt tokeb

---
+++ server/src/auth/dto/auth.dto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsString, MinLength } from 'class-validator'
+
+export class AuthDto {
+  @IsEmail()
+  readonly email: string
+  @MinLength(6, { message: 'Password cannot be less then 6 characters' })
+  @IsString()
+  readonly password: string
+}
