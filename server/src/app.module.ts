e1bbe2b15fee1039d00f02bd9354e37b87f370ca
Fugi <rememberov.andrei@yandex.ru>
2022-10-12 20:29:45 +0300

    feat: create movie Dto and movie Model

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,11 +5,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { getMongoDbConfig } from './config/mongo.config'
-import { UserService } from './user/user.service'
 import { UserModule } from './user/user.module'
 import { GenreModule } from './genre/genre.module';
 import { FileModule } from './file/file.module';
 import { ActorModule } from './actor/actor.module';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
   imports: [
@@ -24,6 +24,7 @@ import { ActorModule } from './actor/actor.module';
     GenreModule,
     FileModule,
     ActorModule,
+    MovieModule,
   ],
   controllers: [AppController],
   providers: [AppService],
