57669628925a6c819133d7982e81aad8545ed6d4
Fugi <rememberov.andrei@yandex.ru>
2022-10-17 00:37:58 +0300

    feat: aggregation movie in actor

--- server/src/actor/actor.service.ts
+++ server/src/actor/actor.service.ts
@@ -31,11 +31,21 @@ export class ActorService {
       }
     }
 
-    // Aggregation
-
-    return this.ActorModel.find(options)
-      .select('-updatedAt -__v')
-      .sort({ createAt: 'desc' })
+    return this.ActorModel.aggregate()
+      .match(options)
+      .lookup({
+        from: 'Movie',
+        foreignField: 'actors',
+        localField: '_id',
+        as: 'movies',
+      })
+      .addFields({
+        countMovies: {
+          $size: '$movies',
+        },
+      })
+      .project({ __v: 0, updatedAt: 0, movies: 0 })
+      .sort({ createAt: -1 })
       .exec()
   }
   // Admin place
