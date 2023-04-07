1af736c911facc070a6a0862c5c00a970a674bd0
4118 Горожанкин А.А <rememberov.andrei@yandex.ru>
2023-04-07 23:37:22 +0300

    postinstall

--- client/app/components/ui/SkeletonLoader.tsx
+++ client/app/components/ui/SkeletonLoader.tsx
@@ -1,7 +1,8 @@
 import cn from 'classnames'
 import { FC } from 'react'
 import Skeleton, { SkeletonProps } from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/Skeleton.css'
+
+// import 'react-loading-skeleton/dist/'
 
 const SkeletonLoader: FC<SkeletonProps> = ({ className, ...rest }) => {
 	return (
